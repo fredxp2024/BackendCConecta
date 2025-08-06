b75352738704fd3ac094587359f9a6852ce0ae24
fredxp2024 <fredycusco2018@gmail.com>
2025-08-05 23:27:55 -0500

    Guardando cambios locales antes del rebase

--- obj/Debug/net8.0/BackendCConecta.AssemblyInfo.cs
+++ obj/Debug/net8.0/BackendCConecta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendCConecta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985b5cf0976acb5b5a6da44df4a215447d4734c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8114eabb5693ac359c84e14d96e9997246733e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendCConecta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendCConecta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
