48724f4c397442ee037a93c7a72205ea96f3f092
fredxp2024 <fredycusco2018@gmail.com>
2025-08-07 14:55:06 -0500

    Update LoginCommandHandler.cs
    login

--- BackendCConecta/BackendCConecta/Aplicacion/Modulos/Auth/Handlers/LoginCommandHandler.cs
+++ BackendCConecta/BackendCConecta/Aplicacion/Modulos/Auth/Handlers/LoginCommandHandler.cs
@@ -1,6 +1,32 @@
-﻿namespace BackendCConecta.Aplicacion.Modulos.Auth.Handlers
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using BackendCConecta.Aplicacion.Modulos.Auth.Comandos;
+using BackendCConecta.Aplicacion.Modulos.Auth.DTOs;
+using BackendCConecta.Aplicacion.Modulos.Auth.Interfaces;
+
+namespace BackendCConecta.Aplicacion.Modulos.Auth.Handlers
+
 {
-    public class LoginCommandHandler
+
+    public class LoginCommandHandler : IRequestHandler<LoginCommand, LoginResponseDto>
     {
+        private readonly IAuthService _authService;
+
+        public LoginCommandHandler(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<LoginResponseDto> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var loginRequest = new LoginRequestDto
+            {
+                Correo = request.CorreoElectronico,
+                Password = request.Password
+            };
+
+            return await _authService.LoginAsync(loginRequest);
+        }
     }
 }
